177deb2708f623c793d4c8b6942d342a726517bd
prateeksha9 <prateeksha.67@gmail.com>
2023-02-16 12:48:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const multer = require("multer");
+const path = require("path");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User with ID: ${socket.id} joined room: ${room}`);
+  });
+
+  socket.on("send_message", (messageData) => {
+    socket.to(messageData.room).emit("receive_message", messageData);
+    console.log(
+      `User with ID: ${socket.id} sent ${messageData.message} at ${messageData.time}`
+    );
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("SERVER RUNNING");
+});
